0d98e1bc474ac02270307e01b63386decb868c98
Alan R Panosso <arpanosso@yahoo.com.br>
2021-06-04 19:29:32 -0300

    aula 09

--- aulaGGPLOT2.R
+++ aulaGGPLOT2.R
@@ -1,13 +1,3 @@
-
-
-
-
-
-
-
-
-
-
 library(tidyverse)
 library(bigrquery)
 
--- patchwork.R
+++ patchwork.R
@@ -20,5 +20,55 @@ g4 +
 
 
 
+options(digits=15)
+x<-63780000+557.92
+x
+
+
+# gganimate ---------------------------------------------------------------
+library(gganimate)
+library(tidyverse)
+library(patchwork)
+library(cowplot)
+
+n=5
+di1 <- 1/n*5
+x1<-seq(-4,4,di1)
+densidade1 <- dnorm(x1)
+
+n=15
+di2 <- 1/n*5
+x2<-seq(-4,4,di2)
+densidade2 <- dnorm(x2)
+
+n=50
+di3 <- 1/n*5
+x3<-seq(-4,4,di3)
+densidade3 <- dnorm(x3)
+
+n=1000
+di4 <- 1/n*5
+x4<-seq(-4,4,di4)
+densidade4 <- dnorm(x4)
+
+tab1 <- tibble(x=x1,dd=densidade1,g=1)
+tab2 <- tibble(x=x2,dd=densidade2,g=2)
+tab3 <- tibble(x=x3,dd=densidade3,g=3)
+tab4 <- tibble(x=x4,dd=densidade4,g=4)
+tab<-rbind(tab1,tab2,tab3,tab4)
+
+tab |>
+    ggplot(aes(x=x,y=dd))+
+    geom_col(color="blue",fill="white")+
+    labs(x="(X)",
+         y="di") +
+  theme_minimal() +
+  #facet_wrap(~g,nrow=3)+
+  exit_disappear() +
+  transition_states(g , wrap = FALSE) +
+  enter_grow() +
+  enter_fade()
+
+
 
 
