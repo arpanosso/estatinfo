bc0c2c75727f7032dfd1a51c33fcc2e7cc67993f
Alan R Panosso <arpanosso@yahoo.com.br>
2021-06-06 20:56:31 -0300

    aula 09

--- 01-Normal.R
+++ 01-Normal.R
@@ -1,66 +1,106 @@
-
-# Modified from https://shiny.rstudio.com/tutorial/written-tutorial/lesson1/
 library(tidyverse)
 library(shiny)
-library(shinythemes)
-data(airquality)
+library(shinydashboard)
 
-# Define UI for app that draws a histogram ----
-ui <- fluidPage(
 
-                # App title ----
-                titlePanel("Estudo de parâmetros Distribuição Normal"),
 
-                # Sidebar layout with input and output definitions ----
-                sidebarLayout(
-                  # Sidebar panel for inputs ----
-                  sidebarPanel(
-                    width = 3,
-                    # Input: Slider for the number of bins ----
-                    sliderInput(inputId = "media",
-                                label = "Valor da média",
-                                min = 0,
-                                max = 50,
-                                value = 25,
-                                ste= 2),
-                    sliderInput(inputId = "variancia",
-                                label = "Valor da variancia",
-                                min = 0,
-                                max = 25,
-                                value = 5)
-
-                  ),
-
-                  # Main panel for displaying outputs ----
-                  mainPanel(
-                    width = 9,
-
-                    # Output: plot ----
-                    plotOutput(outputId = "distPlot")
-
-                  )
-                )
+ui <- dashboardPage(
+  header=dashboardHeader(
+    title = "Estatística-UNESP"
+  ),
+  sidebar=dashboardSidebar(
+    sidebarMenu(
+      menuItem("Normal",tabName = "normal"),
+      menuItem("Binomial",tabName = "binomial"),
+      menuItem("Poisson", tabName = "poisson")
+    )
+  ),
+  body=dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "normal",
+        fluidRow( # sempre usar com Colum, para não dar toque
+          column(h2("Estudo dos Parâmetros da Normal"),width = 12),
+          column(
+            h3("Distribuição 01"),
+            width = 4,
+            sliderInput("m1","Média",
+                        min=0,
+                        max=100,
+                        value = 50,
+                        step = 1),
+            numericInput("sd1","Desvio-padrão",min=5,
+                         max=25,
+                         value = 7,
+                         step = 1)
+          ),
+          column(
+            h3("Distribuição 02"),
+            width = 4,
+            sliderInput("m2","Média",
+                        min=0,
+                        max=100,
+                        value = 50,
+                        step = 1),
+            numericInput("sd2","Desvio-padrão",min=5,
+                         max=25,
+                         value = 7,
+                         step = 1)
+          ),
+          column(
+            h3("Distribuição 03"),
+            width = 4,
+            sliderInput("m3","Média",
+                        min=0,
+                        max=100,
+                        value = 50,
+                        step = 1),
+            numericInput("sd3","Desvio-padrão",min=5,
+                         max=25,
+                         value = 7,
+                         step = 1)
+          ),
+          column(
+            width = 12,
+            plotOutput("grafico_hist")
+          )
+        )
+      ),
+      tabItem(
+        tabName = "binomial",
+        h1("Estudo de parâmetros Binomial")
+      ),
+      tabItem(
+        tabName = "poisson",
+        h1("Estudo de parâmetros Poisson")
+      )
+    )
+  ),
+  title = "Meu App"
 )
 
-# Define server logic required to draw a plot ----
-server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    m<-input$media
-    v<-input$variancia
-    curve(dnorm(x,m,sqrt(v)),0,50,
-          las=1,ylim=c(0,0.28),
-          col="red",
-          lwd=1.2,
-          xlab="variável x",
-          ylab="Densidade",
-          main=paste("N(",m,v,")"))
-    abline(v=input$media,lty=2,col="blue")
 
 
-
+server <- function(input, output, session) {
+  output$grafico_hist <- renderPlot({
+    ggplot(data.frame(x = c(0, 100)), aes(x = x)) +
+      stat_function(fun = \(x) dnorm(x,input$m1,input$sd1),
+                    col="red")+
+      stat_function(fun = \(x) dnorm(x,input$m2,input$sd2),
+                    col="blue") +
+      stat_function(fun = \(x) dnorm(x,input$m3,input$sd3),
+                    col="darkgreen")+
+      theme_bw() +
+      labs(x="Variável aleatória (X)",
+           y="Densidade de frequência")+
+      theme(
+        axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16)
+      )+
+      geom_vline(xintercept = c(input$m1,input$m2,input$m3),
+                 color = "darkgray",lty=2)
   })
 
 }
 
-# Create Shiny app ----
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
