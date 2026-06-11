5d5d63274cc9cb90ba34a7ce445dc1a68c7cafec
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2026-06-10 22:41:50 -0300

    aula 11 final

---
+++ teste.R
@@ -0,0 +1,53 @@
+library(readxl)
+library(dplyr)
+library(openxlsx)
+
+# Pasta onde estão os arquivos
+pasta <- "c:/Users/p4Nn/Downloads/power-query/"
+
+# Lista dos arquivos Excel
+arquivos <- list.files(
+  pasta,
+  pattern = "\\.xlsx$",
+  full.names = TRUE
+)
+
+# Nomes das abas do primeiro arquivo
+abas <- excel_sheets(arquivos[1])
+
+# Lista para armazenar os dados consolidados
+resultado <- list()
+
+for(aba in abas){
+
+  dados_aba <- lapply(arquivos, function(arq){
+
+    read_excel(arq, sheet = aba) |>
+      mutate(arquivo_origem = basename(arq))
+
+  })
+
+  resultado[[aba]] <- bind_rows(dados_aba)
+
+}
+
+# Criar arquivo consolidado
+wb <- createWorkbook()
+
+for(aba in names(resultado)){
+
+  addWorksheet(wb, aba)
+
+  writeData(
+    wb,
+    sheet = aba,
+    resultado[[aba]]
+  )
+
+}
+
+saveWorkbook(
+  wb,
+  "sucupira_consolidado.xlsx",
+  overwrite = TRUE
+)
